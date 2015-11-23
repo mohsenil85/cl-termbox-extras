083039fdde37b208b883aabf6596d7b6f41408ad
Grant Vesely <fouric@users.noreply.github.com>
2015-11-22 17:35:58 -0800

    Streamlined write-text even more

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -1,7 +1,6 @@
 (in-package :termbox-extras)
 
-(defun draw-text (x y text &optional (fg-bg (cons termbox:+default+ termbox:+default+)))
+(defun draw-text (x y text &optional (fg termbox:+default+) (bg termbox:+default+))
   "execute a series of change-cell's in a sequential manner such as to write a line of text"
-  (let ((converted-text (coerce text 'list)))
-    (dotimes (i (length text))
-      (termbox:change-cell (+ x i) y (char-code (nth i converted-text)) (car fg-bg) (cdr fg-bg)))))
+  (dotimes (i (length text))
+    (termbox:change-cell (+ x i) y (char-code (schar text i)) fg bg)))
